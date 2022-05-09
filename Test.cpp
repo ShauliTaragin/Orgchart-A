f99f85191435b5fffe8c1d20e0ab8ce36b5aeb46
ShauliTaragin <shaulitaragin@gmail.com>
2022-05-09 17:17:05 +0300

    started to fill in declarations in header class

--- Test.cpp
+++ Test.cpp
@@ -1,4 +1,19 @@
 //
 // Created by shaul on 09/05/2022.
 //
-
+#include "doctest.h"
+#include "sources/OrgChart.hpp"
+using namespace ariel;
+using namespace std;
+TEST_CASE ("Case 1 Football Team")
+{
+    //checking integers
+    OrgChart chart1{};
+            CHECK_NOTHROW(chart1.add_root("Owner"));
+            CHECK_NOTHROW(chart1.add_root("New_Owner"));
+            CHECK_THROWS(chart1.add_sub("Owner" , "General Manager"));//Owner was replaced
+            CHECK_THROWS(chart1.add_sub("Lebron James" , "General Manager"));//no such value
+            CHECK_NOTHROW(chart1.add_sub("New_Owner" , "General Manager"));
+            CHECK_NOTHROW(chart1.add_sub("New_Owner" , "Share Holders"));
+            CHECK_NOTHROW(chart1.add_sub("New_Owner" , "Head of operations"));
+}
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -1,8 +1,32 @@
 //
 // Created by shaul on 09/05/2022.
 //
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
 
-#ifndef ORGCHART_A_ORGCHART_H
-#define ORGCHART_A_ORGCHART_H
+namespace ariel{
+    class OrgChart{
+    public:
+        OrgChart();
+        OrgChart & add_root(string name);
+        OrgChart & add_sub(string parent , string son);
 
-#endif //ORGCHART_A_ORGCHART_H
+
+        friend std::ostream& operator<< (std::ostream& output, const OrgChart& chart);
+
+    public:
+
+        //-------------------------------------------------------------------
+        // Create a iterator sub_class which will be implemented by inner class
+        //-------------------------------------------------------------------
+        class iterator {
+
+
+        };
+    };
+
+}
+
